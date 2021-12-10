d4e044d3cb289edbd80d4bc2ee9e7b38f87a0937
Stephan Seitz <sseitz@nvidia.com>
2021-12-10 08:08:36 +0100

    Add ZMQSend for fun

---
+++ templates/ZMQSend/source.cpp
@@ -0,0 +1,21 @@
+// TODO: includes
+
+namespace cpp_derive {
+{%- for class in classes %}
+
+size_t send(Connector& connector, const {{ class.name }}& item) {
+  size_t totalBytes = 0;
+
+  {% for name, field in class.fields -%}
+  {%- if field.field_type | striptags == "std::vector" -%}
+  // Not necessary when 
+  totalBytes += connector.sendVector({item.{{ name }}}, zmq::snd_more);
+  {%- else -%}
+  totalBytes += connector.send({item.{{ name }}}, zmq::snd_more);
+  {%- endif %}
+  {% endfor %}
+  return totalBytes;
+}
+{%- endfor %}
+
+}
